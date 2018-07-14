a49a178eaa548dbddc04ec27647c3cc3d62c834e
Robert Schumann <dukfaar@gmail.com>
2018-07-14 10:39:50 +0200

    reorganize files

--- relay/connection.go
+++ relay/connection.go
@@ -27,3 +27,18 @@ func (r *ConnectionResolver) PageInfo() *PageInfoResolver {
 	}
 	return &PageInfoResolver{PageInfo: &p}
 }
+
+func GenerateConnectionTypes(baseType string) string {
+	return `
+	type ` + baseType + `Connection {
+		edges: [` + baseType + `Edge]
+		totalCount: Int
+		pageInfo: PageInfo!
+	}
+
+	type ` + baseType + `Edge {
+		node: ` + baseType + `
+		cursor: ID!
+	}
+	`
+}
--- relay/pageInfo.go
+++ relay/pageInfo.go
@@ -1,6 +1,7 @@
 package relay
 
 import (
+	"github.com/dukfaar/goUtils/service"
 	"github.com/graph-gophers/graphql-go"
 )
 
@@ -39,3 +40,28 @@ type PageInfo {
 	hasPreviousPage: Boolean
 }
 `
+
+func GetHasPreviousAndNextPage(resultLength int, firstItemID string, lastItemID string, service service.DBService) (chan bool, chan bool) {
+	hasPreviousPage := make(chan bool)
+	hasNextPage := make(chan bool)
+
+	go func() {
+		if resultLength > 0 {
+			result, _ := service.HasElementBeforeID(firstItemID)
+			hasPreviousPage <- result
+		} else {
+			hasPreviousPage <- false
+		}
+	}()
+
+	go func() {
+		if resultLength > 0 {
+			result, _ := service.HasElementAfterID(lastItemID)
+			hasNextPage <- result
+		} else {
+			hasNextPage <- false
+		}
+	}()
+
+	return hasPreviousPage, hasNextPage
+}
