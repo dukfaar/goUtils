27c8e4b6ad8cb02a496c9e5a8b9120ecbc09dfda
Robert Schumann <dukfaar@gmail.com>
2018-07-16 11:16:39 +0200

    add Port to ServiceInfo

--- eventbus/serviceInfo.go
+++ eventbus/serviceInfo.go
@@ -3,6 +3,7 @@ package eventbus
 type ServiceInfo struct {
 	Name                  string
 	Hostname              string
+	Port                  string
 	GraphQLHttpEndpoint   string
 	GraphQLSocketEndpoint string
 }
