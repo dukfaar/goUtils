6799b537cfca5a6dfc067674ed42f4d22826aea9
Robert Schumann <dukfaar@gmail.com>
2018-07-19 11:23:46 +0200

    add AuthenticationHandler

--- http/addContext.go
+++ http/addContext.go
@@ -10,3 +10,11 @@ func AddContext(ctx context.Context, h http.Handler) http.Handler {
 		h.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
+
+func Authenticate(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, "Authentication", r.Header.Get("Authentication"))
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
